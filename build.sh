947f486b136f4c36f8fd91e982620bcb0c3dad2d
Robin Swift <robinswiftart@gmail.com>
2023-11-07 22:35:46 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function latex_build () {
+
+    pdflatex $1.tex
+    bibtex $1.aux
+    makeindex $1.ind
+    pdflatex $1.tex
+    pdflatex $1.tex
+    pdflatex $1.tex
+
+}
+
+pandoc -o content.tex ../_index.md
+
+latex_build refman_a4
+latex_build refman_us_letter
