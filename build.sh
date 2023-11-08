09c7a711e4721762a6b85515c3d491fe2373850d
Robin Swift <robinswiftart@gmail.com>
2023-11-08 09:47:45 +0000

    more fixes to Tex code

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+MODE="-interaction=nonstopmode"
+
 function latex_build () {
 
-    pdflatex $1.tex
+    pdflatex $MODE $1.tex
     bibtex $1.aux
     makeindex $1.ind
-    pdflatex $1.tex
-    pdflatex $1.tex
-    pdflatex $1.tex
+    pdflatex $MODE $1.tex
+    pdflatex $MODE $1.tex
+    pdflatex $MODE $1.tex
 
 }
 
-latex_build refman_a4
-latex_build refman_us_letter
+latex_build emrm_alpha_a4
+latex_build emrm_alpha_us_letter
